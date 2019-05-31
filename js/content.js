1377bb7422eecd2520f3f85d4aae2ddbfc1cd86c
F-Sampaio <f.s1488@outlook.com>
2019-05-31 13:06:43 -0400

    Trocada a cor do darkmode, alinhado o botão. Adicionado o FAQ

--- js/content.js
+++ js/content.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#modal-content').load("html/modal.html");
     $('#menu-content').load("html/navbar.html");
     $('#software-content').load("html/software.html");
+    $('#faq-content').load("html/faq.html");
     $('#comprar-content').load("html/comprar.html");
     $('#equipe-content').load("html/equipe.html");
     $('#contato-content').load("html/contato.html");
